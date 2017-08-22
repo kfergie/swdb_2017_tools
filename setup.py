0c21a8b5197cad68b2fd8560b2a72eb29dfb4ebe
Katie Ferguson <kt.ferguson@gmail.com>
2017-08-21 19:58:44 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='swdb_2017_tools'.
+    version='0.1.0',
+    packages=find_packages(),
+    license='Allen Institute Software License'
+)
